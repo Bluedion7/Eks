be114c76d2767feacedc0948c614c2c425dde631
Blue <bluedion7@gmail.com>
2023-04-26 19:49:06 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "cluster_id" {
+  description = "Name of the EKS cluster where the ingress nginx will be deployed"
+  type        = string
+
+}
+
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
